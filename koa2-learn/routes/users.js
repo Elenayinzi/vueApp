b38c3c7f5077582464965492368214e8571c46da
ElenaLiaoYin <elenaliao13827469027@outlook.com>
2019-07-08 08:37:56 +0800

    nuxt.js

--- koa2-learn/routes/users.js
+++ koa2-learn/routes/users.js
@@ -1,4 +1,8 @@
 const router = require('koa-router')()
+const Redis = require('koa-redis')
+const Person = require('../dbs/models/person')
+
+const Store = new Redis().client
 
 router.prefix('/users')
 
@@ -8,6 +12,71 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+router.get('/fix',async function(ctx){
+  const st = await Store.hset('fix','name',Math.random())
+  ctx.body = {
+    code: 0
+  }
+})
+
+router.post('/addPerson', async function(ctx){
+    const person = new Person({
+      name: ctx.request.body.name,
+      age: ctx.request.body.age
+    })
+    let code;
+    try {
+      await person.save();
+      code = 0;
+    } catch (e) {
+      code = -1;
+    }
+    ctx.body = {
+      code: 0
+    }
+})
+
+router.post('/getPerson', async function(ctx){
+  const result = await Person.findOne(
+    {name: ctx.request.body.name} //找到符合条件的就返回了
+  );
+  const resuts = await Person.find( //找到所有的数据
+    {name: ctx.request.body.name}
+  );
+  let code;
+  try {
+    await person.save();
+    code = 0;
+  } catch (e) {
+    code = -1;
+  }
+  ctx.body = {
+    code: 0,
+    result,
+    resuts
+  }
+})
+
+router.post('/updatePerson', async function(ctx){
+    const result = await Person.where({
+      name: ctx.request.body.name
+    }).update({
+      age: ctx.request.body.age
+    })
+    ctx.body = {
+      code: 0
+    }
+})
+
+router.post('/removePerson', async function(ctx){
+  const result = await Person.where({
+    name: ctx.request.body.name
+  }).remove()
+  ctx.body = {
+    code: 0
+  }
 })
 
 module.exports = router
