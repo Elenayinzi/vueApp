9ab3e01b73b10ff3f34a3654c5aef971dba2983b
Elenayinzi <1090054400@qq.com>
2019-07-19 20:01:02 +0800

    首页开发

--- mt-app/pages/index.vue
+++ mt-app/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        mt-app
-      </h1>
       <h2 class="subtitle">
         My bedazzling Nuxt.js project
       </h2>
@@ -29,12 +25,7 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
